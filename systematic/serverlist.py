965485bde4d7bc7f6279e7a12afc934decd36218
Ilkka Tuohela <hile@iki.fi>
2011-11-26 11:35:42 +0300

    Imported server update tool

---
+++ systematic/serverlist.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+"""
+Configuration classes to enumerate servers in an organization, and to run
+basic update, non-interactive and interactive shell commands on the servers.
+
+Designed for usage over ssh, could work with other protocols I guess at 
+least for interactive sessions (start VNC etc).
+"""
+
+import os,logging
+from subprocess import Popen,PIPE
+from configobj import ConfigObj
+
+DEFAULT_CONFIG_PATH = '/etc/servers.list'
+
+class OrganizationServers(dict):
+    def __init__(self,path=DEFAULT_CONFIG_PATH):
+        self.log = logging.getLogger('modules')
+
+        if not os.path.isfile(path):
+            raise ValueError('No such file: %s' % path)
+        try:
+            config  = ConfigObj(path)
+        except ValueError,emsg:
+            raise ValueError('Error parsing %s: %s' % (path,emsg))
+        for k in config.keys():
+            self[k] = OperatingSystemGroup(k,config[k])
+
+class OperatingSystemGroup(list):
+    def __init__(self,name,opts):
+        self.log = logging.getLogger('modules')
+        self.name = name
+        try:
+            self.description = opts['description']
+        except KeyError:
+            self.description = name
+        try:
+            self.connect_command = opts['connect']
+        except KeyError:
+            self.connect_command = ['ssh','-qt','SERVER']
+
+        try:
+            self.command_separator = opts['command_separator']
+        except KeyError:
+            self.command_separator = ' && '
+
+        try:
+            self.update_commands = opts['commands']
+            if len(self.update_commands) == 0:
+                self.update_commands = None
+        except KeyError:
+            self.update_commands = None
+
+        try:
+            self.extend(map(lambda s: ServerConfig(self,s), opts['servers']))
+        except KeyError:
+            pass
+
+class ServerConfig(object):
+    def __init__(self,os,name):
+        self.log = logging.getLogger('modules')
+        self.os = os
+        self.name = name
+        self.connect_command = [
+            x=='SERVER' and name or x for x in os.connect_command
+        ]
+
+    def __repr__(self):
+        return '%s (OS: %s, Update: %s)' % (
+            self.name,
+            self.os.description,
+            self.os.update_commands is not None and 'Yes' or 'No'
+        )
+
+    def check_output(self,command):
+        if type(command) != list:
+            command = [command]
+        cmd = self.connect_command + command
+        p = Popen(cmd,stdout=PIPE,stderr=PIPE)
+        (stdout,stderr) = p.communicate()
+        return (p.returncode,stdout.rstrip('\n'),stderr.rstrip('\n'))
+
+    def shell(self,command):
+        if type(command) != list:
+            command = [command]
+        cmd = self.connect_command + command
+        p = Popen(cmd,stdin=sys.stdin,stdout=sys.stdout,stderr=sys.stderr)
+        p.wait()
+        return p.returncode
+
+    def update(self):
+        if self.os.update_commands is None:
+            self.log.debug('No update commands for OS %s' % self.os.name)
+            return
+        cmd = self.connect_command + [
+            self.os.command_separator.join(self.os.update_commands)
+        ]
+        self.log.debug('Running: %s' % cmd)
+        p = Popen(cmd,stdin=sys.stdin,stdout=sys.stdout,stderr=sys.stderr)
+        p.wait()    
+        return p.returncode
+
+if __name__ == '__main__':
+    import sys
+    logging.basicConfig(level=logging.DEBUG)
+    try:
+        sl = OrganizationServers(sys.argv[1])
+    except IndexError:
+        print 'Usage: %s <servers.list>' % sys.argv[0]
+        sys.exit(1)
+    for name,group in sl.items():
+        for server in group:
+            (rc,stdout,stderr) = server.check_output(sys.argv[2:])
+            if rc != 0:
+                print 'Error running uname on %s:\n%s\n%s' % (
+                    server.name,stdout,stderr
+                )
+                continue 
+            print '%s: %s' % (server.name,stdout)
+            continue
+            if server.shell(sys.argv[2:])!=0:
+                print 'Error running uname on %s' % server.name
+
