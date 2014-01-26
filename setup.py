28fbacddabcf1a2605e691e5fea39300fcfe87fe
Ilkka Tuohela <hile@iki.fi>
2014-01-26 12:54:52 +0200

    Fix formatting of setup.py, fix requirements, bump version

--- setup.py
+++ setup.py
@@ -5,24 +5,30 @@ Platform indepependent tools for sysadmins.
 Platform dependent modules are now split to their own packages.
 """
 
-import sys,os
+import sys
+import os
 from setuptools import setup
 
-VERSION='3.2'
+VERSION='3.3'
 README = open(os.path.join(os.path.dirname(__file__),'README.md'),'r').read()
 
 setup(
     name = 'systematic',
-    keywords = 'System Management Utility Scripts',
+    keywords = 'system management utility scripts',
     description = 'Sysadmin utility classes and scripts',
-    long_description = README, 
-    author = 'Ilkka Tuohela', 
+    long_description = README,
+    author = 'Ilkka Tuohela',
     author_email = 'hile@iki.fi',
     url = 'http://tuohela.net/packages/systematic',
     version = VERSION,
     license = 'PSF',
     zip_safe = False,
-    packages = ['systematic'],
-    install_requires = [ 'setproctitle', 'configobj' ],
-)   
+    packages = (
+        'systematic',
+    ),
+    install_requires = (
+        'setproctitle',
+        'configobj',
+    ),
+)
 
