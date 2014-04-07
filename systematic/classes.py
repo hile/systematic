73f78f244c44de90893991706e3874c401a7249c
Ilkka Tuohela <hile@iki.fi>
2014-04-07 07:18:40 +0300

    Add some common utility classes

---
+++ systematic/classes.py
@@ -0,0 +1,37 @@
+"""
+Common wrapper classes
+"""
+
+class SortableContainer(object):
+    compare_fields = ()
+    def __cmp__(self, other):
+        if self.compare_fields:
+            for field in self.compare_fields:
+                a = getattr(self, field)
+                b = getattr(other, field)
+                print 'compare', a,b
+                if a != b:
+                    return cmp(a, b)
+
+            return 0
+
+        else:
+            return cmp(self, other)
+
+    def __eq__(self, other):
+        return self.__cmp__(other) == 0
+
+    def __ne__(self, other):
+        return self.__cmp__(other) != 0
+
+    def __lt__(self, other):
+        return self.__cmp__(other) < 0
+
+    def __lte__(self, other):
+        return self.__cmp__(other) <= 0
+
+    def __gt__(self, other):
+        return self.__cmp__(other) > 0
+
+    def __gte__(self, other):
+        return self.__cmp__(other) >= 0
