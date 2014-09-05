2c171a90e6dff8d6998a233cc1cc58e3de2ab08a
Ilkka Tuohela <hile@iki.fi>
2014-09-05 09:41:43 +0300

    Bump version to 4.1.0 for logging API changes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-VERSION='4.0.8'
+VERSION='4.1.0'
 README = open(os.path.join(os.path.dirname(__file__),'README.md'),'r').read()
 
 setup(
