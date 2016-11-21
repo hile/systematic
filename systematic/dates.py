96b93976d50b5cca9adbcd8f01a15410b3fe7c5f
Ilkka Tuohela <ilkka.tuohela@codento.com>
2016-11-21 07:33:39 +0200

    Docstring changes only

--- systematic/dates.py
+++ systematic/dates.py
@@ -79,14 +79,17 @@ class Day(object):
             return self.value.isoweekday()
         return getattr(self.value, attr)
 
+    def __str__(self):
+        return self.value.strftime(DEFAULT_DATE_FORMAT)
+
+    def __repr__(self):
+        return self.__str__()
+
     def __long__(self):
         return long(self.value.strftime('%s'))
 
     def __hash__(self):
         return long(self)
-
-    def __str__(self):
-        return self.value.strftime(DEFAULT_DATE_FORMAT)
 
     def __eq__(self, value):
         return long(self) == long(value)
@@ -177,19 +180,20 @@ class Week(object):
 
         self.workdays.sort()
 
-    def __getattr__(self, attr):
-        if attr == 'previous':
-            return self-1
-        if attr == 'next':
-            return self+1
+    def __repr__(self):
+        return self.__str__()
 
-        return getattr(self.first.value, attr)
+    def __str__(self):
+        return 'Week {0} {1} - {2}'.format(self.first.strftime('%U'), self.first, self.last)
 
     def __hash__(self):
         return long(self.first)
 
     def __int__(self):
         self.first.strftime('%U')
+
+    def __getattr__(self, attr):
+        return getattr(self.first.value, attr)
 
     def __getitem__(self, attr):
         try:
@@ -202,18 +206,28 @@ class Week(object):
 
         raise IndexError('Invalid week day index: {0}'.format(attr))
 
-
     def __sub__(self, value):
         return Week(self.first-7*int(value), None, firstweekday=self.firstweekday)
 
     def __add__(self, value):
         return Week(self.first+7*value, None, firstweekday=self.firstweekday)
 
-    def __str__(self):
-        return 'WEEK {0} {1} - {2}'.format(self.first.strftime('%U'), self.first, self.last)
-
     def __iter__(self):
         return self
+
+    @property
+    def previous_week(self):
+        """Previous week
+
+        """
+        return self - 1
+
+    @property
+    def next_week(self):
+        """Previous week
+
+        """
+        return self - 1
 
     def next(self):
         """
@@ -249,6 +263,9 @@ class Month(object):
             self.weeks.append(week)
             week+=1
 
+    def __repr__(self):
+         return self.first.strftime('%B %Y')
+
     def __hash__(self):
         return long(self.first)
 
@@ -263,9 +280,6 @@ class Month(object):
 
         raise IndexError('Invalid month day index: {0} (month has {1:d} days)'.format(attr, self.days))
 
-    def __repr__(self):
-         return self.first.strftime('%B %Y')
-
     def __len__(self):
         return self.days
 
@@ -275,7 +289,7 @@ class Month(object):
     def __add__(self, value):
         m = self
         value = int(value)
-        if value>=0:
+        if value >= 0:
             for i in range(0, int(value)):
                 m = Month(m.first+m.days, None, firstweekday=self.firstweekday)
         else:
